a24262370710917bbb1156597f1d0931fcf2d777
aishao01 <aishao01@163.com>
2026-05-17 15:09:34 +0800

    add: script to patch hb-ft.cc clang cast-function-type error

---
+++ .github/scripts/patch_harfbuzz.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Patch hb-ft.cc in .buildozer to suppress clang cast-function-type-strict errors."""
+import os
+
+buildozer_dir = os.path.expanduser('~/.buildozer')
+count = 0
+for root, dirs, files in os.walk(buildozer_dir):
+    for f in files:
+        if f == 'hb-ft.cc':
+            path = os.path.join(root, f)
+            with open(path) as fh:
+                content = fh.read()
+            if '#pragma clang diagnostic' not in content:
+                pragma = '#pragma clang diagnostic ignored "-Wcast-function-type-strict"\n'
+                with open(path, 'w') as fh:
+                    fh.write(pragma + content)
+                print(f'Patched: {path}')
+                count += 1
+            else:
+                print(f'Already patched: {path}')
+
+print(f'Total files patched: {count}')
