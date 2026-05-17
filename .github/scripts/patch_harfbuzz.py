c8b89d65e12a41959b3c87efc2017784d863f4d8
aishao01 <aishao01@163.com>
2026-05-17 16:05:00 +0800

    fix: use #pragma clang system_header on hb-ft.cc (nuclear option)

--- .github/scripts/patch_harfbuzz.py
+++ .github/scripts/patch_harfbuzz.py
@@ -1,38 +1,35 @@
 #!/usr/bin/env python3
-"""Patch harfbuzz Android.mk to add -Wno-error=cast-function-type-strict."""
+"""Patch hb-ft.cc to mark as system header (suppress ALL warnings from this file)."""
 import os
 
 cwd = os.getcwd()
-print(f'Searching in: {cwd}')
-
+count = 0
 for root, dirs, files in os.walk(cwd):
     for f in files:
-        if f != 'Android.mk':
+        if f != 'hb-ft.cc':
             continue
         path = os.path.join(root, f)
-        if 'harfbuzz' not in path and 'SDL2_ttf' not in path:
+        if 'SDL2_ttf' not in path:
             continue
         
         with open(path) as fh:
             content = fh.read()
         
-        if 'Wno-error=cast-function-type-strict' in content:
+        if '#pragma' in content or 'system_header' in content:
             print(f'Already patched: {path}')
             continue
         
-        lines = content.split('\n')
-        changed = False
-        for i, line in enumerate(lines):
-            if 'LOCAL_CFLAGS' in line and ('+=' in line or ':=' in line):
-                if '-Wno-error=cast-function-type-strict' not in line:
-                    lines[i] = line.rstrip() + ' -Wno-error=cast-function-type-strict'
-                    changed = True
-        
-        if changed:
-            with open(path, 'w') as fh:
-                fh.write('\n'.join(lines))
-            print(f'Patched: {path}')
-        else:
-            print(f'No LOCAL_CFLAGS found in: {path}')
+        # Add system_header pragma at the very top
+        pragma_lines = '''#if defined(__clang__)
+#pragma clang system_header
+#elif defined(__GNUC__) || defined(__GNUG__)
+#pragma GCC system_header
+#endif
 
-print('Done')
+'''
+        with open(path, 'w') as fh:
+            fh.write(pragma_lines + content)
+        print(f'Patched: {path}')
+        count += 1
+
+print(f'Total: {count}')
