0af5a6da6505db53708442b8308bb5911e7e4233
aishao01 <aishao01@163.com>
2026-05-17 15:54:40 +0800

    fix: use Wno-error= instead of Wno- (NDK adds -Werror after LOCAL_CFLAGS)

--- .github/scripts/patch_harfbuzz.py
+++ .github/scripts/patch_harfbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""Patch harfbuzz Android.mk to add -Wno-cast-function-type-strict."""
-import os, glob
+"""Patch harfbuzz Android.mk to add -Wno-error=cast-function-type-strict."""
+import os
 
 cwd = os.getcwd()
 print(f'Searching in: {cwd}')
@@ -16,17 +16,16 @@ for root, dirs, files in os.walk(cwd):
         with open(path) as fh:
             content = fh.read()
         
-        if 'Wno-cast-function-type-strict' in content:
+        if 'Wno-error=cast-function-type-strict' in content:
             print(f'Already patched: {path}')
             continue
         
-        # Add flag to all LOCAL_CFLAGS lines
         lines = content.split('\n')
         changed = False
         for i, line in enumerate(lines):
             if 'LOCAL_CFLAGS' in line and ('+=' in line or ':=' in line):
-                if '-Wno-cast-function-type-strict' not in line:
-                    lines[i] = line.rstrip() + ' -Wno-cast-function-type-strict'
+                if '-Wno-error=cast-function-type-strict' not in line:
+                    lines[i] = line.rstrip() + ' -Wno-error=cast-function-type-strict'
                     changed = True
         
         if changed:
@@ -36,5 +35,4 @@ for root, dirs, files in os.walk(cwd):
         else:
             print(f'No LOCAL_CFLAGS found in: {path}')
 
-# Also just delete hb-ft.cc if patching fails - SDL2_ttf will use fallback
 print('Done')
